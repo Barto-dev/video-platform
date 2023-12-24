e0167be3cd0a0d7a1bf3a4ed0a2aa34b2e231502
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-12-24 08:37:39 +0000

    feat: auth page styling

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import { Logo } from './_components/Logo';
+
+type AuthLayoutProps = {
+  children: ReactNode;
+};
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
