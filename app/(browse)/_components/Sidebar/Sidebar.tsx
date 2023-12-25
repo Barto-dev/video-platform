c335dd0a96762807ae06f9b9047ff4948d823a64
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-12-25 15:39:35 +0000

    feat: add Recommended module

--- app/(browse)/_components/Sidebar/Sidebar.tsx
+++ app/(browse)/_components/Sidebar/Sidebar.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Wrapper } from './Wrapper';
 import { Toggle } from './Toggle';
-import { Recommended } from './Recommended';
+import { Recommended, RecommendedSkeleton } from './Recommended';
 import { getRecommended } from '@/lib/recommended.service';
 
 export const Sidebar = async () => {
-  const recommended = await getRecommended()
+  const recommended = await getRecommended();
 
   return (
     <Wrapper>
       <Toggle />
-      <div className="space-y-4 pt-4 lg:pt-0">
+      <div className="pt-4 space-y-4 lg:pt-0">
         <Recommended data={recommended} />
       </div>
     </Wrapper>
+  );
+};
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside
+      className="fixed left-0 flex flex-col w-collapsed-sidebar lg:w-expanded-sidebar h-full bg-background border-r border-[#2D2E35] z-50">
+      <RecommendedSkeleton />
+    </aside>
   );
 };
