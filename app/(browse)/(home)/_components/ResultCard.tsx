5c731c5a886973e1bfd44bc9bc43a63425367df5
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-01-03 19:04:24 +0000

    feat: add home page

---
+++ app/(browse)/(home)/_components/ResultCard.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { User } from '@prisma/client';
+import Link from 'next/link';
+import { Thumbnail, ThumbnailSkeleton } from '@/components/Thumbnail';
+import { LiveBadge } from '@/components/LiveBadge';
+import { UserAvatar, UserAvatarSkeleton } from '@/components/UserAvatar';
+import { Skeleton } from '@/components/ui/skeleton';
+
+interface ResultCardProps {
+  data: {
+    id: string;
+    user: User;
+    thumbnailUrl: string | null;
+    isLive: boolean;
+    name: string;
+  }
+}
+
+export const ResultCard = ({ data }: ResultCardProps) => {
+  return (
+    <Link href={`/${data.user.username}`}>
+      <div className="h-full w-full space-y-4">
+        <Thumbnail
+          src={data.thumbnailUrl}
+          fallbackSrc={data.user.image}
+          isLive={data.isLive}
+          username={data.user.username}
+        />
+        {data.isLive && (
+          <div className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-1 transition-transform">
+            <LiveBadge>Live</LiveBadge>
+          </div>
+        )}
+        <div className="flex gap-x-3">
+          <UserAvatar
+            imageUrl={data.user.image}
+            userName={data.user.username}
+            isLive={data.isLive}
+          />
+          <div className="flex flex-col text-sm overflow-hidden">
+            <p className="truncate font-semibold hover:text-blue-600">{data.name}</p>
+            <p className="text-muted-foreground">{data.user.username}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export const ResultCardSkeleton = () => {
+  return (
+    <div className="h-full w-full space-y-4">
+      <ThumbnailSkeleton />
+      <div className="flex gap-x-3">
+        <UserAvatarSkeleton />
+        <div className="flex flex-col gap-y-1">
+          <Skeleton className="h-4 w-32" />
+          <Skeleton className="h-3 w-24" />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/(browse)/(home)/_components/Results.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { getStreams } from '@/lib/feed.service';
+import { ResultCard, ResultCardSkeleton } from './ResultCard';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export const Results = async () => {
+  const data = await getStreams();
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Streams we think you&apos;ll like
+      </h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+          {data.map((stream) => (
+            <ResultCard key={stream.id} data={stream} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-muted-foreground text-sm">No Streams found.</div>
+      )}
+    </div>
+  );
+};
+
+export const ResultsSkeleton = () => {
+  return (
+    <div>
+      <Skeleton className="h-8 w-[290px] mb-4" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        <ResultCardSkeleton />
+        <ResultCardSkeleton />
+        <ResultCardSkeleton />
+      </div>
+    </div>
+  );
+};
